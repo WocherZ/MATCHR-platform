69602706fcff9ca2df2b221cdcef6f79b96e0a25
Vladimir <cay108@yandex.ru>
2023-05-24 18:35:42 +0300

    add spring project

---
+++ src/main/java/com/platform/matchr/MatchrApplication.java
@@ -0,0 +1,13 @@
+package com.platform.matchr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platform/matchr/MatchrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.matchr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
